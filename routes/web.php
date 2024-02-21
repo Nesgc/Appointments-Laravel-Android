5bf801115acf57c51d8abdd4141d6069baf76c51
Nesgc <nestorgarciac11@gmail.com>
2024-02-20 21:24:23 -0600

    specialties

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\MemberController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\Specialty;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +31,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/specialties', [Specialty::class, 'index'])->name('specialties');
+    Route::get('/specialties/create', [Specialty::class, 'create'])->name('specialties');
+    Route::get('/specialties/{specialty}/edit', [Specialty::class, 'edit'])->name('specialties');
+    Route::post('/specialties', [Specialty::class, 'store'])->name('specialties');
 });
